9243012e2e564559e782b5a31646688fdcd44690
Soomin Choi <choisium@gmail.com>
2022-03-16 07:39:39 -0700

    assn2: implement basic functionalities of tcp_receiver

--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -57,6 +57,12 @@ class StreamReassembler {
     //! \brief Is the internal state empty (other than the output stream)?
     //! \returns `true` if no substrings are waiting to be assembled
     bool empty() const;
+
+
+    /* Below functions are auxiliary functions used in upper wrapper, tcp_receiver */
+    uint64_t first_unassembled_seqno() const { return next_assembled_index; }
+
+    size_t assembled_bytes() const { return _output.buffer_size(); }
 };
 
 #endif  // SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,26 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const TCPHeader& header = seg.header();
+
+    /* Set the Initial Sequence Number */
+    if (header.syn) {
+        _isn = header.seqno;
+
+        /* Only SYN signal came in */
+        if (seg.length_in_sequence_space() == 0) {
+            _reassembler.push_substring("", 0, header.fin);
+            _ackno = WrappingInt32(0);
+            return;
+        }
+    }
+
+    /* Push payload to reassembler */
+    uint64_t stream_index = unwrap(header.seqno, _isn, _reassembler.first_unassembled_seqno()) - 1;
+    _reassembler.push_substring(seg.payload().copy(), stream_index, header.fin);
+    _ackno = wrap(_reassembler.first_unassembled_seqno() + 1, _isn);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const { return _ackno; }
 
-size_t TCPReceiver::window_size() const { return {}; }
+size_t TCPReceiver::window_size() const { return _capacity - _reassembler.assembled_bytes(); }
